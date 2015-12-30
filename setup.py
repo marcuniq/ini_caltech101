340319c90d10915e10e9ae6c4e4bd90821f3b509
Marco Unternährer <marco.unter@gmail.com>
2015-12-30 17:27:41 +0100

    add scikit-learn as dependency

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='ini_caltech101',
       description='',
       author='Marco Unternaehrer',
       author_email='marco.unter@gmail.com',
-      install_requires=['keras==0.2.0', 'Image'],
+      install_requires=['keras==0.2.0', 'scikit-learn', 'Image'],
       packages=find_packages())
